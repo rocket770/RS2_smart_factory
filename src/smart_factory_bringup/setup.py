2307a9d511289dbf7e3134721d3682e1f5ab4977
rocket770 <skrillx789@hotmail.com>
2026-02-20 15:19:11 -0800

    test

--- src/smart_factory_bringup/setup.py
+++ src/smart_factory_bringup/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='nick',
-    maintainer_email='skrillx789@hotmail.com',
+    maintainer_email='',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
